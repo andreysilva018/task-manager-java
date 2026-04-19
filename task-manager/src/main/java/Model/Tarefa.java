d054e0ef8fda0253d9eaad13bafd2d23f113bd24
MatheusMeloAlvesgit config --global user.email matheusdemelobalves@gmail.comgit config --global user.name MatheusMeloAlves <matheusdemelobalves@gmail.com>
2026-04-19 02:04:57 -0300

    feat:criar classe Tarefa (#4)

--- task-manager/src/main/java/Model/Tarefa.java
+++ task-manager/src/main/java/Model/Tarefa.java
@@ -9,5 +9,30 @@ package Model;
  * @author Windows
  */
 public class Tarefa {
+    private string titulo;
+    private string descricao;
+    private boolean status;
+
+    public Tarefa(string titulo,string descricao){
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.status = false;
+    }
+
+    public string getTitulo(){
+        return titulo;
+    }
+
+    public string getDescricao(){
+        return descricao;
+    }
+
+    public boolean setStatus(){
+        this.status = true;
+    }
+    public boolean getStatus(){
+        return status;
+    }
     
+
 }
